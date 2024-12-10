f6c237d818a7db5cc93519a1722cce4cdedbc2bb
prabhjotsumman <prabhjot.nith@gmail.com>
2024-12-09 19:19:54 -0700

    icons added

---
+++ soar/src/assets/icons/icons.ts
@@ -0,0 +1,27 @@
+import dollar from "./dollar.svg";
+import card from "./card.svg";
+import paypal from "./paypal.svg";
+import home from "./home.svg";
+import transfer from "./transfer.svg";
+import accounts from "./user.svg";
+import investment from "./economic-investment.svg";
+import creditCard from "./credit-card.svg";
+import loan from "./loan.svg";
+import services from "./service.svg";
+import privileges from "./econometrics.svg";
+import settings from "./settings.svg";
+
+export const ICONS = {
+  DOLLAR: dollar,
+  CARD: card,
+  PAYPAL: paypal,
+  HOME: home,
+  TRANSFER: transfer,
+  ACCOUNTS: accounts,
+  INVESTMENT: investment,
+  CREDIT_CARD: creditCard,
+  LOAN: loan,
+  SERVICES: services,
+  PRIVILEGES: privileges,
+  SETTINGS: settings,
+};
