76a900e279eeed81a68beb9973db560765d5e541
prabhjotsumman <prabhjot.nith@gmail.com>
2024-12-10 02:17:30 -0700

    App updated

--- soar/src/App.tsx
+++ soar/src/App.tsx
@@ -1,21 +1,27 @@
-// import { useState } from 'react'
 import "./App.css";
 
 import Header from "./components/Header/Header";
 import MyCards from "./components/MyCards/MyCards";
 import RecentTransactions from "./components/RecentTransactions/RecentTransactions";
 import Sidebar from "./components/Sidebar/Sidebar";
+import QuickTransfer from "./QuickTransfer/QuickTransfer";
 
 function App() {
   return (
     <>
       <div className="App">
-        <div className=" border-orange-400 border-4">
-          <Header />
-          <div className="flex">
-            <Sidebar />
-            <MyCards />
-            <RecentTransactions />
+        <Header />
+        <div className=" border-orange-400 border-4 flex bg-slate-400">
+          <Sidebar />
+          <div className="flex-col border-2 border-emerald-500 ">
+            <div className="flex">
+              <MyCards />
+              <RecentTransactions />
+            </div>
+            <div className="p-8">
+
+            <QuickTransfer />
+            </div>
           </div>
         </div>
       </div>
