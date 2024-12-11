262133c40569d3c5847d379523481b140251672d
prabhjotsumman <prabhjot.nith@gmail.com>
2024-12-11 02:12:58 -0700

    / points to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import Dashboard from "./Dashboard/Dashboard.tsx";
 import Overview from "./Dashboard/Overview.tsx";
 import Settings from "./Dashboard/Settings.tsx";
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
+        <Route index element={<Dashboard />} />
         <Route path="dashboard" element={<Dashboard />}>
           <Route path="overview" element={<Overview />} />
           <Route path="settings" element={<Settings />} />
