55d77002ca630fcd113506ade7077e24a2bb2e13
prabhjotsumman <prabhjot.nith@gmail.com>
2024-12-11 02:01:10 -0700

    vercel plugin removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
 });
