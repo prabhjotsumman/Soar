5464753f10eee5b91bddfac7200d0f1ccc44622a
prabhjotsumman <prabhjot.nith@gmail.com>
2024-12-11 02:20:48 -0700

    default view set to overview

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
 
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import Overview from "./Overview";
 import Settings from "./Settings";
 
 function Dashboard() {
+  const location = useLocation();
+  const pathname = location.pathname;
   return (
     <>
       <div className="w-screen bg-white xs:bg-roar-cream sm:bg-roar-cream">
@@ -15,8 +17,10 @@ function Dashboard() {
 
           {/* Display area */}
           <div className="flex flex-col w-full p-6">
+            {/* Default display Overview */}
+            {pathname === "/" && <Overview />}
             <Routes>
-              <Route path="overview" element={<Overview />} />
+              <Route index path="overview" element={<Overview />} />
               <Route path="settings" element={<Settings />} />
             </Routes>
           </div>
