ad9ad9a37ef044c04ad3b6e3480fea33bcf9697d
prabhjotsumman <prabhjot.nith@gmail.com>
2024-12-11 19:43:16 -0700

    main updated to point to Dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
+import './App.css';
 import Dashboard from "./Dashboard/Dashboard.tsx";
 import Overview from "./Dashboard/Overview.tsx";
 import Settings from "./Dashboard/Settings.tsx";
@@ -13,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <div className="bg-black">
         <Routes>
-          <Route index element={<App />} />
+          <Route index element={<Dashboard />} />
           <Route path="dashboard" element={<Dashboard />}>
             <Route path="overview" element={<Overview />} />
             <Route path="settings" element={<Settings />} />
